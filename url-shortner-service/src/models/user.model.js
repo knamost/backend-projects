9693fde95ec0cf23ff01ec169ffe0e1962718d2d
knamost <sumansenpai@gmail.com>
2026-02-18 23:58:35 +0545

    added user model.js

---
+++ url-shortner-service/src/models/user.model.js
@@ -0,0 +1,14 @@
+import { pgTable, varchar, uuid, text, timestamp } from "drizzle-orm/pg-core";
+
+
+export const usersTable = pgTable("users", {
+    id: uuid().primaryKey().defaultRandom(),
+    firstname: varchar('first_name', { length: 55 }).notNull(),
+    lastname: varchar('last_name', {length: 55}),
+    email: varchar({length: 255}).notNull().unique(),
+    password: text().notNull(),
+
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at').$onUpdate(() => new Date()),
+});
+
